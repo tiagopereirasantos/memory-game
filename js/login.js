bde1d1e63eee2a3f6391116a9133e55ff7bf63d7
Tiago Pereira Santos <tiago03081997@gmail.com>
2022-11-15 19:06:45 -0300

    Finalização do css do game e cartas

--- js/login.js
+++ js/login.js
@@ -2,6 +2,11 @@ var loginInput = document.querySelector(".login-input");
 var loginButton = document.querySelector(".login-button");
 var loginForm = document.querySelector(".login-form");
 
+/*
+const audio = new Audio('meu_audio.mp3');
+audio.play();
+*/
+ 
 function validateInput (event){
   var inputLength = event.target.value.length;
   if (inputLength > 2 ){
@@ -20,4 +25,4 @@ function handleSubmit(event){
 }
 
 loginInput.addEventListener("input", validateInput);
-loginForm.addEventListener("submit", handleSubmit);
+loginForm.addEventListener("submit", handleSubmit);d
